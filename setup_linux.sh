9450ba7fec4dd35ed6be84c13350bf4b7ff3a990
RFL <163516891+Tristan-3@users.noreply.github.com>
2025-07-01 17:52:22 +0200

    Delete setup_linux.sh

--- setup_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Instalando dependencias de Node.js..."
-npm install express node-fetch
-
-echo "Instalando dependencias de Python..."
-pip3 install requests beautifulsoup4
-
-echo "Instalación completa."
